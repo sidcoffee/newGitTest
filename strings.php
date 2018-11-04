b93864d7e98c6deb813403762c8af1a4db8ffb28
Siddharth Seetharaman <35478416+sidcoffee@users.noreply.github.com>
2018-11-04 11:43:56 +0530

    Create strings.php

---
+++ strings.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <title>My Coffee</title>
+        <body>
+        <?php 
+        
+        $first  =   "The quick brown fox";
+        $second =   "Jumps over the lazy dog";
+
+        $third  =   $first;
+        $third  .=  $second;
+
+        echo $third;
+        
+        ?>
+        <br />
+        Lowercase: <?php    echo strtolower($third);   ?> <br />
+        Uppercase: <?php    echo strtoupper($third);   ?> <br />           
+        Ucfirst: <?php    echo ucfirst($third);   ?> <br />          
+        Ucwords: <?php    echo ucwords($third);   ?> <br />           
+        <br />
+        Strlen : <?php    echo strlen($third);   ?> <br />
+        Trim: <?php    echo trim($third);   ?> <br />           
+        Ucfirst: <?php    echo strstr($third);   ?> <br />          
+        Ucwords: <?php    echo ucwords($third);   ?> <br />      
+        </body>
+    </head>
+</html>
